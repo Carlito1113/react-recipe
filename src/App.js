c926faafd511a9bbf9b8b8c20435efd5961b3673
Carlito Algarin <carlito1113@yahoo.com>
2021-06-11 20:49:47 -0400

    initial api setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import ApiButton from './components/ApiButton'
 
 function App() {
   return (
     <div className="App">
       <h1>Recipe Lads</h1>
+      <ApiButton />
     </div>
   );
 }
---
+++ src/components/ApiButton.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import axios from 'axios';
+
+export default function ApiButton() {
+  async function callApi() {
+    try {
+      const recipe = await axios.get(
+        `https://api.spoonacular.com/recipes/findByIngredients?ingredients=apples,+flour,+sugar&number=2&apiKey=${process.env.REACT_APP_API_KEY_SPOONACULAR}`
+      );
+      console.log(recipe)
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  callApi();
+
+  return (
+    <div>
+      <button>Call API</button>
+    </div>
+  );
+}
