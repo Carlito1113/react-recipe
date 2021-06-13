1126a4f725881f7453668ba3d15db2785769c634
Christian Marquez <christianmarquez41@gmail.com>
2021-06-12 22:03:29 -0400

    displayed recipe title

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,10 +1,16 @@
 import React from 'react'
 
-export default function Recipes() {
+export default function Recipes({ recipes }) {
 
   return (
     <div>
-     <h1>This is the Recipe Component</h1> 
+      <h1>This is the Recipe Component</h1>
+      {recipes.map((recipe, recipeIdx) => {
+        console.log("inside loop")
+        return (
+          <div key={recipeIdx}>{recipe.title}</div>
+        )
+      })}
     </div>
   )
 }
