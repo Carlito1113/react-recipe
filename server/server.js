8d90e10a2c870f717b24f6434005afafc924d828
Carlito Algarin <carlito1113@yahoo.com>
2021-06-25 18:39:15 -0400

    commit to merge new changes

--- server/server.js
+++ server/server.js
@@ -1,4 +1,3 @@
-
 const express = require('express')
 const mongoose = require("mongoose")
 const app = express()
@@ -15,22 +14,8 @@ app.use(express.json())
 
 mongoose.connect(process.env.MONGODB_URI || process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
-db.on("error", err => console.error(error))
+db.on("error", err => console.error(err))
 db.once("open", () => console.log("connected to db"))
-=======
-const express = require('express');
-const mongoose = require('mongoose');
-const app = express();
-const port = 3001;
-require('dotenv').config();
-
-mongoose.connect(process.env.MONGODB_URI || process.env.DATABASE_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-const db = mongoose.connection;
-db.on('error', err => console.error(error));
-db.once('open', () => console.log('connected to db'));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
