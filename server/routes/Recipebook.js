19fc859bc5b475ca46a8603352bdbb15c1b6d2ca
Christian Marquez <christianmarquez41@gmail.com>
2021-06-25 17:09:15 -0400

    created post route and get route for recipes

---
+++ server/routes/Recipebook.js
@@ -0,0 +1,35 @@
+const express = require("express")
+const router = express.Router()
+const Recipebook = require("../models/recipe")
+
+// Getting all recipes
+router.get("/", async (req, res) => {
+    try {
+        const recipes = await Recipebook.find()
+        res.send(recipes)
+    }
+    catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+})
+
+// Save a recipe
+router.post("/", async (req, res) => {
+    const recipe = new Recipebook({
+        recipeName: req.body.recipeName,
+        image: req.body.image,
+        preparationMinutes: req.body.preparationMinutes,
+        readyInMinutes: req.body.readyInMinutes,
+        servings: req.body.servings,
+        sourceUrl: req.body.sourceUrl,
+        summary: req.body.summary
+    })
+
+    try {
+        const newRecipe = await recipe.save()
+        res.status(201).json(newRecipe)
+    }
+    catch (err) {
+        res.status(400).json({ message: err.message })
+    }
+})
