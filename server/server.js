7d00bcb8ed0530fcb0058362848bd40c7455e24e
Carlito Algarin <carlito1113@yahoo.com>
2021-06-26 00:33:50 -0400

    commit for merge

--- server/server.js
+++ server/server.js
@@ -12,15 +12,9 @@ const db = mongoose.connection;
 db.on('error', err => console.error(error));
 db.once('open', () => console.log('connected to db'));
 
-<<<<<<< HEAD
-app.get('/', (req, res) => {
-  res.send('Hellasdasdo!');
-});
-=======
 const recipebookRouter = require("./routes/Recipebook")
 
 app.use("/api/recipebook", recipebookRouter)
->>>>>>> a3d50b58c2de1f290b5e0e273d2229458dc34aed
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
