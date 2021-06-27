7805eacc2e8de476e1e03d48d10caab8f0536fb8
Christian Marquez <christianmarquez41@gmail.com>
2021-06-27 15:55:00 -0400

    added encryption for user's password

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,37 +1,33 @@
 const router = require("express").Router();
-const User = require("../models/user")
-const bcrypt = require("bcryptjs")
-const { registerValidation } = require("../validation")
+const User = require("../models/user");
+const bcrypt = require("bcryptjs");
+const { registerValidation } = require("../validation");
 
 router.post("/register", async (req, res) => {
-    // Input Validation
-    const { error } = registerValidation(req.body)
-    if (error) return res.status(400).send(error.details[0].message)
-    
-    // Check if user already exists
-    const emailExists = await User.findOne({userName: req.body.userName})
-    if(emailExists) return res.status(400).send("Username already exists")
+  // Input Validation
+  const { error } = registerValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
-    //Hash password
-    const hash = await
-    
-    // Create New User
-    const user = new User({
-        userName: req.body.userName,
-        password: req.body.password,
-    })
-    try {
-        const savedUser = await user.save()
-        res.send(savedUser)
-    }
-    catch (err) {
-        res.status(400).send(err)
-    }
-})
+  // Check if user already exists
+  const emailExists = await User.findOne({ userName: req.body.userName });
+  if (emailExists) return res.status(400).send("Username already exists");
 
+  //Hash password
+  const hashedPassword = await bcrypt.hash(req.body.password, 10);
 
-router.post("/login", (req, res) => {
+  // Create New User
+  const user = new User({
+    userName: req.body.userName,
+    password: hashedPassword,
+  });
+  try {
+    const savedUser = await user.save();
+    res.send({ user: user._id });
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
 
-})
+router.post("/login", (req, res) => {});
 
-module.exports = router
+module.exports = router;
